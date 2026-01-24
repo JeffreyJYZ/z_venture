4872b8eaa645cb41545ff9e427ffa2b8e66a8f11
Jeffrey <yizhoujiang11@gmail.com>
2026-01-24 15:43:59 +0400

    CSS adjustments to /

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,43 @@ import continueGame from "@/app/actions/continueGame";
 import newGame from "@/app/actions/newGame";
 import StateText from "@/app/ui/components/stateText";
 
+const BtnStyles: React.CSSProperties = {
+	height: "100px",
+	padding: "20px",
+	borderRadius: "30px",
+	fontSize: "20px",
+	marginTop: "20px",
+	backgroundColor: "#a80000",
+};
+
 export default function Home() {
 	const [stateC, actionC, pendingC] = useActionState(continueGame, null);
 	const [stateN, actionN, pendingN] = useActionState(newGame, null);
 	return (
 		<Container>
 			<h1>Z Venture</h1>
-			<form action={actionC}>
-				<button type="submit" disabled={pendingN || pendingC}>
-					{pendingC ? "loading..." : "Continue Game"}
-				</button>
-				{!!stateC && <StateText>{stateC}</StateText>}
-			</form>
-			<form action={actionN}>
-				<button type="submit" disabled={pendingN || pendingC}>
-					{pendingN ? "loading..." : "New Game"}
-				</button>
-				{!!stateN && <StateText>{stateN}</StateText>}
-			</form>
+			<section className="flex">
+				<form action={actionC}>
+					<button
+						type="submit"
+						disabled={pendingN || pendingC}
+						style={BtnStyles}
+					>
+						{pendingC ? "loading..." : "Continue Game"}
+					</button>
+					{!!stateC && <StateText>{stateC}</StateText>}
+				</form>
+				<form action={actionN}>
+					<button
+						type="submit"
+						disabled={pendingN || pendingC}
+						style={BtnStyles}
+					>
+						{pendingN ? "loading..." : "New Game"}
+					</button>
+					{!!stateN && <StateText>{stateN}</StateText>}
+				</form>
+			</section>
 		</Container>
 	);
 }
