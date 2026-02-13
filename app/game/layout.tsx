073439c29c34a3d4e84b39952a4a4d22181ba0ea
Jeffrey <yizhoujiang11@gmail.com>
2026-02-13 10:02:11 +0400

    added gamename to nav title

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -2,9 +2,14 @@ import Container from "../ui/components/container";
 import CustomTopLoader from "../ui/components/customTopLoader";
 import { toNavLinks, URLs } from "../../utils/data/urls";
 import Navbar from "../ui/components/navBar";
-import { isCurrentTokenExpired } from "../../utils/funcs/dbFuncs";
+import {
+	getGameState,
+	getLastGameId,
+	isCurrentTokenExpired,
+} from "../../utils/funcs/dbFuncs";
 import { cinzelFont } from "../ui/fonts";
 import { unauthorized } from "next/navigation";
+import { isError } from "../../utils/funcs/isRetryableError";
 
 export default async function GameLayout({
 	children,
@@ -12,9 +17,17 @@ export default async function GameLayout({
 	children: React.ReactNode;
 }) {
 	if (await isCurrentTokenExpired()) unauthorized();
+	const gameId = await getLastGameId();
+	if (isError(gameId)) throw gameId.error;
+	const game = await getGameState(gameId);
+	if (isError(game)) throw game.error;
+	const GameName = game?.name;
 	return (
 		<>
-			<Navbar links={toNavLinks(URLs.game)} title="Z Venture" />
+			<Navbar
+				links={toNavLinks(URLs.game)}
+				title={`Z Venture (${GameName ?? "[Unknown Game]"})`}
+			/>
 			<Container
 				className={"flex flex-col p-5 gap-5 " + cinzelFont.className}
 			>
